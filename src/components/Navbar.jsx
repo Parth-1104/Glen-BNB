73a0cbe7bb0e7b86ccee2cb72bd6fd53bf086a7b
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2025-06-13 19:50:17 +0530

    Added clerk login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import {assets} from "../assets/assets"
 
+import { SignedIn, SignedOut, SignInButton, useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 
 
 
+const BookIcon = ()=>(
+    <svg className="w-4 h-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" />
+</svg>
+)
 
 const Navbar = () => {
     const navLinks = [
@@ -16,6 +22,12 @@ const Navbar = () => {
     ];
 
     
+    const { openSignIn } = useClerk();
+
+    const {user}=useUser()
+
+    const navigate=useNavigate();
+    const location=useLocation();
 
     const [isScrolled, setIsScrolled] = React.useState(false);
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -52,9 +64,25 @@ const Navbar = () => {
                 {/* Desktop Right */}
                 <div className="hidden md:flex items-center gap-4">
                    <img src={assets.searchIcon} alt="" className={`${isScrolled&& "invert"}h-7 transition-all duration-500`} />
-                    <button className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
-                        Login
-                    </button>
+                    
+                    {user?
+                    (<UserButton>
+                        <UserButton.MenuItems>
+                            <UserButton.Action label="My Bookings" labelIcon={<BookIcon/>}
+                            onClick={()=>
+                                navigate('/my-bookings')
+
+                            }
+                            />
+                        </UserButton.MenuItems>
+                        </UserButton>)
+                :
+                (<button onClick={() => openSignIn()} className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
+                Login
+            </button>)
+                    }
+                    
+                    
                 </div>
 
                 {/* Mobile Menu Button */}
@@ -80,7 +108,7 @@ const Navbar = () => {
                         Dashboard
                     </button>
 
-                    <button className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                    <button onClick={() => openSignIn()} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
                     </button>
                 </div>
