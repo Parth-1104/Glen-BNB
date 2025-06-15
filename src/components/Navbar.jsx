8afb49f5b7ebcd36ad5c763a75c5110640145be8
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2025-06-15 18:24:09 +0530

    added the custom card

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,7 +56,10 @@ const Navbar = () => {
                             <div className={`${isScrolled ? "bg-gray-700" : "bg-white"} h-0.5 w-0 group-hover:w-full transition-all duration-300`} />
                         </a>
                     ))}
-                    <button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-black'} transition-all`}>
+                    <button onClick={()=>
+                    navigate("/owner")
+                    }
+                    className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-black'} transition-all`}>
                        Dashboard
                     </button>
                 </div>
@@ -86,13 +89,27 @@ const Navbar = () => {
                 </div>
 
                 {/* Mobile Menu Button */}
+                
+
                 <div className="flex items-center gap-3 md:hidden">
+                {user&&<UserButton>
+                        <UserButton.MenuItems>
+                            <UserButton.Action label="My Bookings" labelIcon={<BookIcon/>}
+                            onClick={()=>
+                                navigate('/my-bookings')
+
+                            }
+                            />
+                        </UserButton.MenuItems>
+                        </UserButton>}
                     <img onClick={()=>{
                         setIsMenuOpen(!isMenuOpen)
                     }} src={assets.menuIcon} alt="" className={`${isScrolled && "invert"}h-4`} />
                 </div>
 
                 {/* Mobile Menu */}
+
+                
                 <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
                     <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
                         <img src={assets.closeIcon} alt="" className="h-6.5" />
@@ -104,13 +121,16 @@ const Navbar = () => {
                         </a>
                     ))}
 
-                    <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all">
+                    {user&&<button onClick={()=>
+                    navigate("/owner")
+                    }
+                     className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all">
                         Dashboard
-                    </button>
+                    </button>}
 
-                    <button onClick={() => openSignIn()} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                    {!user&&<button onClick={() => openSignIn()} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
-                    </button>
+                    </button>}
                 </div>
             </nav>
     );
