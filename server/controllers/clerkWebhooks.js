e5cca614c8e4081eeb7f818aa8932b28634e4335
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2025-06-18 21:23:30 +0530

    added clerk

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,47 +1,42 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
 
-// API Controller Function to Manage Clerk User with database
 // POST /api/clerk
 const clerkWebhooks = async (req, res) => {
   try {
-    // Create a Svix instance with clerk webhook secret.
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-    // Getting Headers
     const headers = {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     };
 
-    // Verifying Headers
-    await whook.verify(JSON.stringify(req.body), headers);
-
-    // Getting Data from request body
-    const { data, type } = req.body;
+    const event = await whook.verify(JSON.stringify(req.body), headers);
+    const { data, type } = event;
 
     const userData = {
-      _id: data.id,
-      email: data.email_addresses[0].email_address,
-      username: data.first_name + " " + data.last_name,
-      image: data.image_url,
+      clerkId: data.id, // Don't use _id
+      email: data.email_addresses[0]?.email_address,
+      firstName: data.first_name,
+      lastName: data.last_name,
+      image: data.image_url || data.profile_image_url,
     };
 
-    // Switch Cases for differernt Events
     switch (type) {
       case "user.created": {
-        await User.create(userData);
+        const exists = await User.findOne({ clerkId: data.id });
+        if (!exists) await User.create(userData);
         break;
       }
 
       case "user.updated": {
-        await User.findByIdAndUpdate(data.id, userData);
+        await User.findOneAndUpdate({ clerkId: data.id }, userData);
         break;
       }
 
       case "user.deleted": {
-        await User.findByIdAndDelete(data.id);
+        await User.findOneAndDelete({ clerkId: data.id });
         break;
       }
 
@@ -49,10 +44,10 @@ const clerkWebhooks = async (req, res) => {
         break;
     }
 
-    res.json({ success: true, message: "Webhook Recieved" });
+    res.status(200).json({ success: true, message: "Webhook received" });
   } catch (error) {
-    console.log(error.message);
-    res.json({ success: false, message: error.message });
+    console.error("Webhook error:", error.message);
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema(
-  {
-    _id: { type: String, required: true },
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    image: { type: String, required: true },
-    role: { type: String, enum: ["user", "hotelOwner"], default: "user", },
-    recentSearchedCities: [{ type: String, required: true }],
-  }, { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  clerkId: { type: String, required: true, unique: true },
+  firstName: String,
+  lastName: String,
+  email: { type: String, required: true },
+  image: String,
+  role: { type: String, enum: ["user", "hotelOwner"], default: "user" },
+  recentSearchedCities: [String],
+}, { timestamps: true });
+
 
 const User = mongoose.model("User", userSchema);
 
